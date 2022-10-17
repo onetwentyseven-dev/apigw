5563ac8d8a8b8a3913816e3f902e3ca20f292902
David <48367195+ddouglas@users.noreply.github.com>
2022-10-17 03:27:35 +0000

    simple debugging

--- apigw.go
+++ apigw.go
@@ -1,13 +1,49 @@
 package apigw
 
-import "github.com/sirupsen/logrus"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/sirupsen/logrus"
+)
 
 type Service struct {
-	logger *logrus.Logger
+	logger   *logrus.Logger
+	handlers map[string]Handler
 }
+
+type Handler func(context.Context, events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
 
 func New(lgr *logrus.Logger) *Service {
 	return &Service{
-		logger: lgr,
+		logger:   lgr,
+		handlers: make(map[string]Handler),
 	}
+}
+
+func (s *Service) AddHandler(key string, handler Handler) {
+	if _, ok := s.handlers[key]; ok {
+		s.logger.WithField("key", key).Fatal("handler already registered for key")
+	}
+
+	fmt.Println("adding handler")
+
+	s.handlers[key] = handler
+}
+
+func (s *Service) HandleRoutes() Handler {
+	fmt.Println("Handle Routes called")
+	return func(ctx context.Context, input events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		fmt.Println("event received")
+		if _, ok := s.handlers[input.Resource]; !ok {
+			return s.RespondJSON(http.StatusNotFound, map[string]string{"error": fmt.Sprintf("Route Not Found for %s", input.Resource)}, nil)
+
+		}
+
+		return s.handlers[input.Resource](ctx, input)
+
+	}
+
 }
